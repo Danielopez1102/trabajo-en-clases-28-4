67f222593f12f651532603af2fcd5436156e26c9
Danielopez1102 <daniel.espinoza.lopez@academiacuatropuntocero.com>
2022-04-29 13:30:33 -0600

    mi trabajo 28-4

---
+++ index1.js
@@ -0,0 +1,50 @@
+alerta ('Esta funcionando');
+
+//suma 1
+
+function suma_de_tres_numeros (numero1,numero2,numero3){
+    console.log(numero1+numero2+numero3);
+}
+
+suma_de_tres_numeros(5,5,5);
+
+//multiplicacion 2
+
+function multiplique_tres_numeros (numero1,numero2,numero3){
+    console.log(numero1+numero2+numero3);
+}
+
+suma_de_tres_numeros(5,5,5);
+
+//Un saludo para el profe 3
+
+function un_saludo_al_profe(){
+    console.log ('hey profe, ve que chiva esta funcion')
+}
+
+//if con variable let 4
+
+function podemos_ir_al_supermercado (necesitamos_algo){
+    let indicacion_de_mama = supermercado(necesitamos_algo)
+
+    if (indicacion_de_mama >= si){
+        console.log('Si necesitamos algo');
+    }else{
+        console.log('No necesitamos nada')
+    }
+}
+
+// 5
+
+function invitados(){
+    numeros_invitados_local = numeros_invitados;
+   
+    numeros_invitados_local = 5+5;
+   
+    numeros_invitados_local = numeros_invitados_local * 3;
+    numeros_invitados = numeros_invitados_local;
+   
+   }
+   
+   invitados();
+   
